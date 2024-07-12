d54dd890724b18453c0f3c1b3a903be594029e37
Wins9393 <57756579+Wins9393@users.noreply.github.com>
2024-07-12 13:09:41 +0200

    Delete back/js-build directory

--- back/js-build/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./adminRoutes");
-require("./authRoutes");
-require("./completeFormationRoutes");
-require("./uploadRoutes");
-require("./userStatsRoutes");
